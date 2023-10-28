287b0c645415c89a9c4d38a3051b8da81da59c33
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-28 22:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 function shortcut(s1, s2) {
+    let result = "";
 
-if( s1.length > 0 && s2.length > 0)
-	{
-		return s1.[0] + s2.[0];
-	}
-	else{
-		return "";
-	}
+    // Loop through s1 to find the first non-space character
+    for (let i = 0; i < s1.length; i++) {
+        let currChar1 = s1[i];
+        if (currChar1 !== " ") {
+            result += currChar1;
+            break; // You should break out of the loop once you find a non-space character
+        }
+    }
+
+    // Loop through s2 to find the first non-space character
+    for (let i = 0; i < s2.length; i++) {
+        let currChar2 = s2[i];
+        if (currChar2 !== " ") {
+            result += currChar2;
+            break; // You should break out of the loop once you find a non-space character
+        }
+    }
+
+    // Check if the result is not " " (space) and return it, or return " "
+    if (result !== " ") {
+        return result;
+    } else {
+        return " ";
+    }
 }
+
+// Example usage:
+  
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
