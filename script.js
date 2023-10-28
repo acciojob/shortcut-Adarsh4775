67675c26b6e5e80e7c4af05008ba41fd9ebdc2b2
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-28 22:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
 function shortcut(s1, s2) {
-  // your code here
-	for( i=0; i<s1.length; i++)
-		{
-			let currChar1 = s1.[i];
-			if(currChar1 !== " ")
-			{
-				return currChar1;
-			}
-			else
-			{
-				return " ";
-			}
-			
-		
-	for(i=0; i<s2.length; i++)
-		{
-			let currChar2 = s2.[i];
-			if( currChar2 !== " ")
-			{
-				return currChar2;
-			}
-			else
-			{
-				return " ";
-			}
-		}
-		}
-	if(s1 !== " " || s2 === " "  && s1 === " " || s2 !== " " )
+
+if( s1.length > 0 && s2.length > 0)
 	{
-		return " ";
+		return s1.[0] + s2.[0];
+	}
+	else{
+		return "";
 	}
 }
-shortcut(); 
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));   
- 
+alert(shortcut(s1, s2));
+     
